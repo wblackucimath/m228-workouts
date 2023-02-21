d6aa7a61c9438ec1f28c21a4d1e7b0c1fec77872
William Black <w.black@uci.edu>
2023-02-21 14:46:18 -0800

    using scipy expected value

--- m228_workouts/option_price.py
+++ m228_workouts/option_price.py
@@ -1,9 +1,18 @@
 import numpy as np
+from scipy.stats import binom
 
 
-def binom_euro():
+def binom_euro_call(r, u, d, S0, K, T, n, h):
+    q = 0.5
+
+    expected = binom.expect(h, (n, q))
+
+    return expected
+
+
+def binom_usa_call(r, u, d, S0, K, T, n, h):
     pass
 
 
-def binom_usa():
-    pass
+if __name__ == "__main__":
+    print(binom_euro_call(1,1,1,1,1,1,5, lambda x: x))
