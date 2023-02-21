0eddc8a3105298f8e78deacb792fdfb703683f2e
William Black <w.black@uci.edu>
2023-02-20 18:07:06 -0800

    Completed problem 3 workout 1

--- m228-workouts/psets/problem_3.py
+++ m228-workouts/psets/problem_3.py
@@ -1,31 +1,79 @@
 import yfinance as yf
 from seaborn import displot, histplot
 import matplotlib.pyplot as plt
+from scipy.stats import gaussian_kde, norm
+from scipy.optimize import curve_fit
+from importlib_resources import files
+import numpy as np
 
 
-def solution_3(period="2y"):
-    price_df = yf.download("^GSPC", period=period, interval="1d")[["Close"]]
-    R = price_df.pct_change()
+def solution_3(period="2y", show_plt=False, save_plt=True, fname=None):
+    plt.rcParams["text.usetex"] = True
+    plt.figure(figsize=(8, 4.5))
+    price_df = yf.download(
+        "^GSPC",
+        period=period,
+        interval="1d",
+    )[["Close"]]
 
-    plot = histplot(
-        R,
+    R = price_df.pct_change().dropna()
+
+    def kde(x):
+        return gaussian_kde(R.values.T).pdf(x.T)
+
+    def pdf(x):
+        return norm.pdf(
+            x,
+            loc=R.mean(),
+            scale=R.std(),
+        )
+
+    X_plot = np.linspace(R.min(), R.max(), 500)
+
+    plt.plot(
+        X_plot,
+        kde(X_plot),
+        c="orange",
+    )
+    plt.plot(
+        X_plot,
+        pdf(X_plot),
+        c="r",
+    )
+
+    histplot(
+        data=R,
         stat="density",
         x="Close",
-        kde=True,
-        legend=True,
     )
+
+    plt.grid()
+
+    plt.title("SPX Data with Gaussian Kernel Density Estimator")
+
     plt.xlabel("Return Amount")
     plt.ylabel("Density")
-    plot.legend(
+
+    plt.legend(
         [
-            "Kernel Density Estimate",
-            "Return Amount",
-        ]
+            f"Gaussian Kernel Density Estimate",
+            f"Naive Gaussian from Data\n (Mean: ${float(R.mean()):.2e}$, $\\sigma^2 = {float(R.std()):.2e}$)",
+            f"Return Amount",
+        ],
+        loc="upper left",
     )
+
     plt.tight_layout()
-    plt.show()
+
+    if save_plt:
+        if fname is None:
+            fname = "SPX_Histogram.png"
+        plt.savefig(files("m228-workouts.plots").joinpath(fname))
+    if show_plt:
+        plt.show()
+
     plt.close()
 
 
 if __name__ == "__main__":
-    solution_3("10y")
+    solution_3("5y", show_plt=True)
