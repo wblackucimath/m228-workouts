95534fa12294402f36e721c6daf17929c5c9c7a8
William Black <w.black@uci.edu>
2023-02-20 16:08:24 -0800

    Generate plot

---
+++ m228-workouts/psets/problem_3.py
@@ -0,0 +1,31 @@
+import yfinance as yf
+from seaborn import displot, histplot
+import matplotlib.pyplot as plt
+
+
+def solution_3(period="2y"):
+    price_df = yf.download("^GSPC", period=period, interval="1d")[["Close"]]
+    R = price_df.pct_change()
+
+    plot = histplot(
+        R,
+        stat="density",
+        x="Close",
+        kde=True,
+        legend=True,
+    )
+    plt.xlabel("Return Amount")
+    plt.ylabel("Density")
+    plot.legend(
+        [
+            "Kernel Density Estimate",
+            "Return Amount",
+        ]
+    )
+
+    plt.show()
+    plt.close()
+
+
+if __name__ == "__main__":
+    solution_3("10y")
